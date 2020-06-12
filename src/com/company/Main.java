b6318759d262b445de621b6d0e37d9a5c23afd01
ernisklm@gmail.com <ernisklm@gmail.com>
2020-06-12 14:02:26 +0600

    Исправление ошибок и упорядочивание исполнения условий, согласно поставленной задачи.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void printIt() {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите Ваш возраст: ");
@@ -14,19 +14,34 @@ public class Main {
         System.out.print("Введите температуру на улице: ");
         int temperature = sc2.nextInt();
 
-
-        if ((age > 20 && age < 45) && (temperature > -20 && temperature < 30)) {
+        if ((age >= 20 && age <= 45) && (temperature > -20 && temperature < 30)) {
             System.out.println("Можно идти гулять");
+            System.out.println();
         }
 
-        if (age <= 20 && (temperature >= 0 && temperature <= 28)) {
+        else if (age < 20 && (temperature >= 0 && temperature <= 28)) {
             System.out.println("Можно идти гулять");
+            System.out.println();
         }
 
-        if (age >= 45 && (temperature >= -10 && temperature <= 25)) {
+        else if ((age > 45) && (temperature > -10 && temperature < 25)) {
             System.out.println("Можно идти гулять");
-        } else {
+            System.out.println();
+        }
+        else {
             System.out.println("Оставайтесь дома!");
+            System.out.println();
         }
+
+    }
+
+    public static void main(String[] args) {
+
+        printIt();
+        printIt();
+        printIt();
+        printIt();
+        printIt();
+
     }
 }
